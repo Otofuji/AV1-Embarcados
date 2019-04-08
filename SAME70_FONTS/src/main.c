363fcaa42e89ebfa5c0c0f9da3b94a819ca249e0
Eric Fernando Otofuji Abrantes <ericfoa@al.insper.edu.br>
2019-04-08 16:13:27 -0300

    Mais erros de compilacao incluidos

--- SAME70_FONTS/src/main.c
+++ SAME70_FONTS/src/main.c
@@ -19,7 +19,11 @@
 
 #include "asf.h"
 #include <math.h>
-
+#include <asf.h>
+#include "tfont.h"
+#include "sourcecodepro_28.h"
+#include "calibri_36.h"
+#include "arial_72.h"
 /************************************************************************/
 /* defines                                                              */
 /************************************************************************/
@@ -80,6 +84,35 @@ void RTT_Handler(void)
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+struct ili9488_opt_t g_ili9488_display_opt;
+
+void configure_lcd(void){
+	/* Initialize display parameter */
+	g_ili9488_display_opt.ul_width = ILI9488_LCD_WIDTH;
+	g_ili9488_display_opt.ul_height = ILI9488_LCD_HEIGHT;
+	g_ili9488_display_opt.foreground_color = COLOR_CONVERT(COLOR_WHITE);
+	g_ili9488_display_opt.background_color = COLOR_CONVERT(COLOR_WHITE);
+
+	/* Initialize LCD */
+	ili9488_init(&g_ili9488_display_opt);
+	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+	
+}
+
+
+void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
+	char *p = text;
+	while(*p != NULL) {
+		char letter = *p;
+		int letter_offset = letter - font->start_char;
+		if(letter <= font->end_char) {
+			tChar *current_char = font->chars + letter_offset;
+			ili9488_draw_pixmap(x, y, current_char->image->width, current_char->image->height, current_char->image->data);
+			x += current_char->image->width + spacing;
+		}
+		p++;
+	}
+}
 
 void pin_toggle(Pio *pio, uint32_t mask){
   if(pio_get_output_data_status(pio, mask))
@@ -134,7 +167,13 @@ int main(void){
   
   // Inicializa RTT com IRQ no alarme.
   f_rtt_alarme = true;
+    board_init();
+    sysclk_init();
+    configure_lcd();
     
+    font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
+    font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
+    font_draw_text(&arial_72, "102456", 50, 200, 2);
   // super loop
   // aplicacoes embarcadas não devem sair do while(1).
   while (1){
